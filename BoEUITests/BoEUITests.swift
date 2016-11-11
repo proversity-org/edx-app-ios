744ce0b43354daa0339b78abb872a1646a0468db
jagonzalr <antonio@proversity.org>
2016-11-11 12:30:50 +0200

    update UITets

--- BoEUITests/BoEUITests.swift
+++ BoEUITests/BoEUITests.swift
@@ -17,10 +17,9 @@ class BoEUITests: XCTestCase {
         
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
     }
     
     override func tearDown() {
@@ -31,6 +30,12 @@ class BoEUITests: XCTestCase {
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let app = XCUIApplication()
+        snapshot("splash")
+        app.buttons["login"].tap()
+        snapshot("login")
+        app.navigationBars["Sign In"].buttons["Close"].tap()
     }
     
 }
