6562f852703f7725ebe9d4c3d674b258386d5c86
Jose Antonio Gonzalez <antonio@proversity.org>
2017-02-02 13:42:01 +0200

    update assets

--- Source/OEXLoginSplashViewController.m
+++ Source/OEXLoginSplashViewController.m
@@ -38,6 +38,8 @@
     [super viewDidLoad];
     
     [self.signInButton setTitle:[Strings loginSplashSignIn] forState:UIControlStateNormal];
+    [self.signInButton setTitleColor:[self.environment.styles primaryBaseColor] forState:UIControlStateNormal];
+    [self.signInButton setTitleColor:[self.environment.styles primaryBaseColor] forState:UIControlStateSelected];
     [self.signUpButton applyButtonStyle:[self.environment.styles filledPrimaryButtonStyle] withTitle:[Strings loginSplashSignUp]];
     self.signUpButton.exclusiveTouch = YES;
 }
