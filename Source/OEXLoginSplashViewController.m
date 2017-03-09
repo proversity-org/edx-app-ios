12b666ab486f3627aeaf9cead4e9480b31a3ab5f
Jose Antonio Gonzalez <antonio@proversity.org>
2017-03-09 12:01:41 +0200

    styling citb app [ci ignore]

--- Source/OEXLoginSplashViewController.m
+++ Source/OEXLoginSplashViewController.m
@@ -37,6 +37,9 @@
     [super viewDidLoad];
     
     [self.signInButton setTitle:[Strings loginSplashSignIn] forState:UIControlStateNormal];
+    [self.signInButton setTintColor:[self.environment.styles primaryBaseColor]];
+    [self.signInButton setTitleColor:[self.environment.styles primaryBaseColor] forState:UIControlStateNormal];
+    [self.signInButton setTitleColor:[self.environment.styles primaryBaseColor] forState:UIControlStateSelected];
     [self.signUpButton applyButtonStyle:[self.environment.styles filledPrimaryButtonStyle] withTitle:[Strings loginSplashSignUp]];
 }
 
