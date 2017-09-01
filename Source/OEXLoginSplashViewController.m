db1e53f1918c4ee3e5cb72cacd374ee31e97d0b2
Jose Antonio Gonzalez <antonio@proversity.org>
2017-09-01 13:15:49 +0200

    use black color on link [ci skip]

--- Source/OEXLoginSplashViewController.m
+++ Source/OEXLoginSplashViewController.m
@@ -38,6 +38,9 @@
     
     [self.signInButton setTitle:[Strings loginSplashSignIn] forState:UIControlStateNormal];
     [self.signUpButton applyButtonStyleWithStyle:[self.environment.styles filledPrimaryButtonStyle] withTitle:[Strings loginSplashSignUp]];
+    [self.signInButton setTitleColor:self.environment.styles.neutralBlack forState:UIControlStateNormal];
+    [self.signInButton setTitleColor:self.environment.styles.neutralBlack forState:UIControlStateFocused];
+    [self.signInButton setTitleColor:self.environment.styles.neutralBlack forState:UIControlStateSelected];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
