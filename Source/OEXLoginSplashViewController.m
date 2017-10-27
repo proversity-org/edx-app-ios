d4035ea2cf23ee39ad161bd8a3905bbf07ef18fa
Jose Antonio Gonzalez <antonio@proversity.org>
2017-10-27 09:35:05 +0200

    change btn colors [ci skip]

--- Source/OEXLoginSplashViewController.m
+++ Source/OEXLoginSplashViewController.m
@@ -38,9 +38,6 @@
     
     [self.signInButton setTitle:[Strings loginSplashSignIn] forState:UIControlStateNormal];
     [self.signUpButton applyButtonStyleWithStyle:[self.environment.styles filledPrimaryButtonStyle] withTitle:[Strings loginSplashSignUp]];
-    [self.signInButton setTitleColor:self.environment.styles.neutralBlack forState:UIControlStateNormal];
-    [self.signInButton setTitleColor:self.environment.styles.neutralBlack forState:UIControlStateFocused];
-    [self.signInButton setTitleColor:self.environment.styles.neutralBlack forState:UIControlStateSelected];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
