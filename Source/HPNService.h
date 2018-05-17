a452954b539435a54ee37e8b6ba05fc8d2ad955a
Jose Antonio Gonzalez <antonio@proversity.org>
2018-05-17 13:29:45 +0000

    [Houston] New iOS build c54fc9a2-3cda-11e8-b467-0ed5f89f718b

---
+++ Source/HPNService.h
@@ -0,0 +1,21 @@
+//
+//  HPNService.h
+//  edX
+//
+//  Created by José Antonio González on 2018/05/17.
+//  Copyright © 2018 edX. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef void (^onComplete)(NSDictionary * __nullable data, NSError * __nullable error);
+
+@interface HPNService : NSObject
+
++ (id __nonnull)instance;
+
+- (void)subscribe:(NSString * __nonnull)authKey
+      WithPayload:(NSDictionary * __nonnull)payload
+CompletionHandler:(onComplete __nullable)completionHandler;
+
+@end
