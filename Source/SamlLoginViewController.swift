49a79e151937e6f157e847d5cbecd9269119c587
andrey <andrey.canon@edunext.co>
2018-11-01 10:45:19 -0500

    remove conditions in url and spaces

--- Source/SamlLoginViewController.swift
+++ Source/SamlLoginViewController.swift
@@ -9,19 +9,19 @@
 import WebKit
 
 @objc class SamlLoginViewController: UIViewController, UIWebViewDelegate {
-    
+
     var webView: UIWebView!
     var sessionCookie: HTTPCookie!
     private let loadingIndicatorView = SpinnerView(size: .Large, color: .Primary)
-    
+
     typealias Environment = OEXConfigProvider & OEXStylesProvider & OEXRouterProvider
     fileprivate let environment: Environment
-    
+
     init(environment: Environment) {
         self.environment = environment
         super.init(nibName: nil, bundle :nil)
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -39,11 +39,11 @@ import WebKit
         let closeButton = UIBarButtonItem(image: UIImage(named: "ic_cancel"), style: .plain, target: self, action: #selector(navigateBack))
         navigationItem.leftBarButtonItem = closeButton
     }
-    
+
     func navigateBack() {
         dismiss(animated: true, completion: nil)
     }
-    
+
     func webViewDidStartLoad(_ webView: UIWebView) {
         loadingIndicatorView.startAnimating()
         view.addSubview(loadingIndicatorView)
@@ -52,63 +52,64 @@ import WebKit
         }
         webView.isHidden = true
     }
-    
+
     func webViewDidFinishLoad(_ webView: UIWebView) {
         navigationItem.title = webView.stringByEvaluatingJavaScript(from: "document.title")
         loadingIndicatorView.removeFromSuperview()
         guard let url = webView.request?.URLString else {
             return
         }
-        let userAllowed = !(url.contains(find: "register") || url.contains(find: "login"))
-        if (url.contains(find: (environment.config.apiHostURL()?.absoluteString)!) && userAllowed) {
+        if url.contains(find: (environment.config.apiHostURL()?.absoluteString)!) {
             guard  let cookies = HTTPCookieStorage.shared.cookies else {
                 return
             }
             for cookie in cookies {
-                if (cookie.name.elementsEqual("sessionid")){
+                if cookie.name.elementsEqual("sessionid"){
                     self.sessionCookie = cookie
                     getUserDetails(sessionCookie: cookie)
-                    
+
                 }
             }
         } else {
             webView.isHidden = false
         }
-        
+
     }
-    
-    func handleSuccessfulLoginWithSaml(userDetails: OEXUserDetails) {        
-        
+
+    func handleSuccessfulLoginWithSaml(userDetails: OEXUserDetails) {
+
         guard let session = OEXSession.shared() else {
             return
         }
         session.saveCookies(sessionCookie, userDetails: userDetails)
         self.dismiss(animated: false, completion: nil)
-       
+
     }
-    
+
     //// This methods is used to get user details when user session cookie is available
     func getUserDetails(sessionCookie: HTTPCookie) {
         let config = URLSessionConfiguration.default
         let session = URLSession.init(configuration: config, delegate: nil, delegateQueue: nil)
         let cookie = String(format: "%@=%@", sessionCookie.name, sessionCookie.value)
-        let request = NSMutableURLRequest.init(url: URL(string: String(format: "%@%@", (environment.config.apiHostURL()?.absoluteString)!, URL_GET_USER_INFO))!)        
+        let request = NSMutableURLRequest.init(url: URL(string: String(format: "%@%@", (environment.config.apiHostURL()?.absoluteString)!, URL_GET_USER_INFO))!)
         request.addValue(cookie, forHTTPHeaderField: "Cookie")
         let task = session.dataTask(with: request as URLRequest, completionHandler: self.completionGetUserDetails)
         task.resume()
     }
-    
+
     func completionGetUserDetails(data:Data?, response: URLResponse?, error: Error?) {
         guard error == nil && data != nil else {
             return
         }
         if let httpResponse = response as? HTTPURLResponse {
-            if (httpResponse.statusCode == 200) {
+            if httpResponse.statusCode == 200 {
                 let dictionary = try? JSONSerialization.jsonObject(with: data!)
                 let userDetails = OEXUserDetails(userDictionary: dictionary as! [AnyHashable : Any])
                 handleSuccessfulLoginWithSaml(userDetails: userDetails)
+            } else if httpResponse.statusCode == 401 {
+                webView.isHidden = false
             }
         }
     }
-    
+
 }
